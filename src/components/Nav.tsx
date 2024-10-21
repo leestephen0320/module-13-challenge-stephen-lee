1669765b73bb80989204a9f3a75e8c4d56dad889
Stephen Lee <leestephen0320@gmail.com>
2024-10-20 21:33:56 -0700

    added necessary code to display the navigation bar and link between pages

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -1,7 +1,36 @@
+import { Link, useLocation } from 'react-router-dom';
+
 const Nav = () => {
   // TODO: Add necessary code to display the navigation bar and link between the pages
+  const currentPage = useLocation().pathname;
+
   return (
-    <div>Nav</div>
+    <nav>
+      <ul className='nav nav-tabs'>
+        <li className='nav-item'>
+          <h2>
+            <Link
+              to='/'
+              className={currentPage === '/' ? 'nav-link active' : 'nav-link'}
+            >
+              HOME
+            </Link>
+          </h2>
+        </li>
+        <li className='nav-item'>
+          <h2>
+            <Link
+              to='/SavedCandidates'
+              className={
+                currentPage === '/SavedCandidates' ? 'nav-link active' : 'nav-link'
+              }
+            >
+              POTENTIAL CANDIDATES
+            </Link>
+          </h2>
+        </li>
+      </ul>
+    </nav>
   )
 };
 
