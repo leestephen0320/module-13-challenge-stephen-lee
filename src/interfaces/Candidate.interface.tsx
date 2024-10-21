fea68ecd6d2a76e56dfcb4da31472bf9234a10f0
Stephen Lee <leestephen0320@gmail.com>
2024-10-20 22:03:41 -0700

    started candidateSearch page

--- src/interfaces/Candidate.interface.tsx
+++ src/interfaces/Candidate.interface.tsx
@@ -1,1 +1,10 @@
 // TODO: Create an interface for the Candidate objects returned by the API
+// ? not the exact syntax of the json, check later
+export default interface Candidate {
+    readonly AvatarURL: string | null;
+    readonly Login: string | null;
+    readonly Location: string | null;
+    readonly Email: string | null;
+    readonly Company: string | null;
+    readonly Bio: string | null;
+}
--- src/pages/CandidateSearch.tsx
+++ src/pages/CandidateSearch.tsx
@@ -1,7 +1,35 @@
 import { useState, useEffect } from 'react';
 import { searchGithub, searchGithubUser } from '../api/API';
 
+import type Candidate from '../interfaces/Candidate.interface';
+
 const CandidateSearch = () => {
+  //! the function part of the page
+  //! ==========================================
+
+  // Add Candidate interface to interface to state variable and set intial state obj
+  const [currentCandidate, setCurrentCandidate] = useState<Candidate>({
+    AvatarURL: '',
+    Login: '',
+    Location: '',
+    Email: '',
+    Company: '',
+    Bio: '',
+  });
+
+  // Functino for adding Candidate to potential candidate list
+  const addToPotential = () => {
+    let parsedCandidates:Candidate[] = [];
+    const storedCandiates = localStorage.getItem('potentialCandidates');
+    if (typeof storedCandiates === 'string') {
+      parsedCandidates = JSON.parse(storedCandiates);
+    }
+    parsedCandidates.push(currentCandidate);
+    localStorage.setItem('potentialCandidates', JSON.stringify(parsedCandidates));
+  }
+
+  //! ==========================================
+  //! the html part of the page
   return <h1>CandidateSearch</h1>;
 };
 
